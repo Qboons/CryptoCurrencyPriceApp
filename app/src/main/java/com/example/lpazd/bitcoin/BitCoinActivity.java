33c8e3a2857ab3529851f44ae425115841a0256f
qboons <jakkoz91@gmail.com>
2018-06-08 10:37:24 +0200

    first

---
+++ app/src/main/java/com/example/lpazd/bitcoin/BitCoinActivity.java
@@ -0,0 +1,105 @@
+package com.example.lpazd.bitcoin;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class BitCoinActivity extends AppCompatActivity {
+
+    public static final String BPI_ENDPOINT = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    private OkHttpClient okHttpClient = new OkHttpClient();
+    private ProgressDialog progressDialog;
+    private TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bit_coin_acvivity);
+        txt = findViewById(R.id.txt);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Loading");
+        progressDialog.setMessage("Wait");
+    }
+
+    private static final String TAG = "***MainActivity***";
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_load) {
+            load();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void load() {
+        Request request = new Request.Builder().url(BPI_ENDPOINT).build();
+        progressDialog.show();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(BitCoinActivity.this, "Error during loading data: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String body = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+                        pareeBpiResponse(body);
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void pareeBpiResponse(String body) {
+        Log.v(TAG, body);
+        try {
+            StringBuilder builder = new StringBuilder();
+            JSONObject jsonObject = new JSONObject(body);
+            JSONObject timeObject = jsonObject.getJSONObject("time");
+            builder.append(timeObject.getString("updated")).append("\n\n");
+
+            JSONObject bpiObject = jsonObject.getJSONObject("bpi");
+            JSONObject usdObject = bpiObject.getJSONObject("USD");
+            builder.append(usdObject.getString("rate")).append("$").append("\n");
+
+            JSONObject gbpObject = bpiObject.getJSONObject("GBP");
+            builder.append(gbpObject.getString("rate")).append("Ł").append("\n");
+
+            JSONObject eurObject = bpiObject.getJSONObject("EUR");
+            builder.append(eurObject.getString("rate")).append("E").append("\n");
+
+            txt.setText(builder.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
